9a773affe81fab35be733a3e8c32cfc8612c4fbc
t_yamo <t_yamo@unknown-artifacts.info>
2013-12-17 17:01:48 +0900

    add nginx

--- Berksfile
+++ Berksfile
@@ -3,4 +3,5 @@ cookbook 'user'
 cookbook 'openssh'
 cookbook 'simple_iptables', git: "git://github.com/dcrosta/cookbook-simple-iptables.git"
 cookbook 'nfs'
+cookbook 'nginx'
 
--- roles/dev_server.rb
+++ roles/dev_server.rb
@@ -3,6 +3,25 @@ name :dev_server
 run_list(
   "role[base]",
   "recipe[dev_server]",
-  "recipe[gitolite]"
+  "recipe[gitolite]",
+  "recipe[nginx]"
+)
+
+default_attributes(
+  :nginx => {
+    :version => "1.4.4",
+    :install_method => "source",
+    :source => {
+      :version => "1.4.4",
+      :modules => %w[
+        nginx::http_ssl_module
+        nginx::http_gzip_static_module
+        nginx::http_stub_status_module
+        nginx::http_perl_module
+#        nginx::http_realip_module
+#        nginx::passenger
+      ]
+    }
+  }
 )
 
